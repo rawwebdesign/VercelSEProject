0299977a9e11551dc6a3ec3c84b4113dc43fda12
Bradley Spencer <rawwebdesign@gmail.com>
2025-07-05 23:10:02 -0400

    middleware.ts now provides a redirect

--- vercel-se-project/middleware.ts
+++ vercel-se-project/middleware.ts
@@ -1,6 +1,14 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-//
-// export default function middleware(request: NextRequest) {
-//   return NextResponse.next();
-// }
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+
+  const searchTermFromQuery = searchParams.get("search");
+
+  if (searchTermFromQuery) {
+    return NextResponse.redirect(
+      new URL(`/topic/${searchTermFromQuery}`, request.url),
+    );
+  }
+}
